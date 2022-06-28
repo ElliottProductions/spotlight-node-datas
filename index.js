37e0ad43f7b3efa3e85088e5a316ce629ca4413c
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-28 10:01:56 -0700

    first thing happening

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ class LinkedList {
     this.head = new Node(val);
   }
 
-  add(){
-
+  add(val){
+    let current = this.head;
+    while (current.next){
+      current = current.next;
+    }
+    current.next = new Node(val);
   }
   removeTail(){}
 
